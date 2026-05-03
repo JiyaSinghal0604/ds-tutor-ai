59e6b68e872bbc0116fed24b4299596ca9f0ebdd
JiyaSinghal0604 <jiya.singhal513651@gmail.com>
2026-05-03 21:59:41 +0530

    gemini model call

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import google.generativeai as genai
 app = FastAPI()          #create app
 
 #Test route
@@ -10,10 +11,21 @@ def home():
 #Request body structure
 class ChatRequest(BaseModel):
     query: str       #tells backend-User will send JSON with a query str
+    api_key: str
 
 #Chat endpoint
 @app.post("/chat")
 def chat(request: ChatRequest):
-    return{
-        "response": f"You asked: {request.query}"
-    }
+    try:
+        # Configure Gemini with user's API key (BYOK)
+        genai.configure(api_key=request.api_key)
+
+        #Load model
+        model=genai.GenerativeModel("models/gemini-1.5-flash")
+
+        #Generate response
+        response=model.generate_content(request.query)
+
+        return {"response": response.text}
+    except Exception as e:
+        return {"error": str(e)}
